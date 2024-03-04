caaea863b9e3ad25cf454f97dd5817e0cb42620e
Alex Zarauza <azarauza@nexusgeographics.com>
2024-03-04 17:24:01 +0100

    Feature - Routing + LazyLoad de Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './shared/pages/homePage/homePage.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule) // Carrega el modul de countries que ja té dins seu el seu propi routing.
+  },
+  {
+    path: '**', // Cualquier ruta del path que no sea las anteriores, irá directamente al home
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes) // forRoot es pel modul principal
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/shared/pages/about-page/about-page.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-about-page',
+    templateUrl: './about-page.component.html',
+    styles: []
+})
+export class AboutPageComponent { }
