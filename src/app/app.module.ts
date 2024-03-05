5db5127e7a11a52946e38c42b88c73735e922b1f
Alex Zarauza <azarauza@nexusgeographics.com>
2024-03-05 10:00:33 +0100

    Feature - Cercador de country inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -10,8 +11,9 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [],
